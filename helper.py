a72a179fedbf108910f2459850d743a1ffa94aa3
LucasOSilvaTaak <lucas.silva@taak.com.br>
2026-03-03 21:54:20 -0300

    refactor: rename allData class to AllData and enhance statistics calculation with new methods for class creation and data structuring

--- helper.py
+++ helper.py
@@ -8,13 +8,19 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-class allData:
+class AllData:
     media = 0
     mediana = 0
     variancia = 0
     desvio_padrao = 0
     coeficiente_de_variacao = 0
     tabela = pd.DataFrame()
+    k = 0
+    h = 0
+    H = 0
+    tamanho = 0
+    amplitude = 0
+
 
 class Helper:
     
@@ -24,7 +30,7 @@ class Helper:
 
     def calculate_statistics(self, df: pd.DataFrame, column_name: str, has_header: bool):
         
-        allData = allData()
+        allData = AllData()
         
         if has_header and column_name:
             tempDf = df[column_name]
@@ -33,12 +39,46 @@ class Helper:
 
         tempDf = pd.to_numeric(tempDf, errors="coerce").dropna().sort_values()
         
-        allData.tabela = self.prepare_final_table(tempDf)
+        allData.tabela = self.prepare_final_table(allData, tempDf)
         
-        return allData()
+        return allData
     
-    def prepare_final_table(self, tempDf: pd.Series):
+    def prepare_final_table(self, allData: AllData, tempDf: pd.Series):
         
         tabela = pd.DataFrame()
         
-        return tabela
+        allData.tamanho = len(tempDf)
+        
+        k = int(np.ceil(np.sqrt(allData.tamanho)))
+        
+        allData.k = k
+        
+        allData.H = tempDf.max() - tempDf.min()
+        
+        allData.h = math.ceil(allData.H / k)
+        
+        allData.amplitude = tempDf.max() - tempDf.min()
+        
+        allData.classes = self.create_classes(tempDf, k)
+        
+        allData.tabela = pd.DataFrame(allData.classes, columns=['min', 'max'])
+        
+        print(allData.tabela)
+        
+        return allData.tabela
+    
+    def create_classes(self, tempDf: pd.Series, k: int):
+        classes = []
+        start = int(tempDf.min())
+        i = 0
+        while start < tempDf.max():
+            
+            temp = start
+            
+            start = start + k
+            
+            classes.append([temp , start])
+            
+            i += 1
+            
+        return classes
